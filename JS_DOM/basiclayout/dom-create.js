9eed271ac27f7f75af56fc0252a4f3e09ae55ea2
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2022-09-06 22:18:22 +0530

    06Sep

---
+++ JS_DOM/basiclayout/dom-create.js
@@ -0,0 +1,24 @@
+// var parentElement = document.getElementById("test");
+
+// var target = document.getElementById("abc");
+
+// var find = parentElement.contains(target);
+
+// console.log(find);
+
+
+// hasAttribute
+
+// var target = document.getElementById("test");
+
+// var find = target.hasAttribute("class")
+
+// console.log(find);
+
+// childnodes
+
+var target = document.getElementById('test');
+
+var find = target.hasChildNodes();
+
+console.log(find);
---
+++ JS_DOM/basiclayout/dom-equal.js
@@ -0,0 +1,6 @@
+var target1 = document.getElementById("list1").firstElementChild;
+var target2 = document.getElementById("list2").children[0];
+
+var equal = target1.isEqualNode(target2)
+
+console.log(equal);
