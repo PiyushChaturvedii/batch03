4241ed2cbad892a161ef132118e3afd0113ade0c
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2022-09-22 20:54:04 +0530

    sep22 updated

--- Beg-JS/JSProject/project1/script.js
+++ Beg-JS/JSProject/project1/script.js
@@ -3,11 +3,16 @@ var index = 0;
 function changeColors() {
     var colors = ["red", "blue", "orange", "yellow", "green", "purple"];
     
+    
     document.getElementsByTagName("body")[0].
+    
         style.background = colors[index++];
     
-    console.log(index);
-
+    // console.log(index);
     if (index > colors.length - 1)
         index = 0;
 }
+
+
+a = document.getElementsByTagName("body")[0];
+console.log(a);
