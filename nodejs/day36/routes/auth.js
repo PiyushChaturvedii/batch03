77df3e81173b4651d00f6e619c8dc762510a2cf5
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-02-02 22:02:10 +0530

    auth&validation

--- nodejs/day36/routes/auth.js
+++ nodejs/day36/routes/auth.js
@@ -1,48 +1,71 @@
-const express = require("express");
-const authController = require("../controllers/auth");
-const { check, body } = require("express-validator/check");
+const express = require('express');
+const { check, body } = require('express-validator/check');
+
+const authController = require('../controllers/auth');
+const User = require('../models/user');
+
 const router = express.Router();
-const User= require('../models/user')
 
-router.get("/login", authController.getLogin);
-router.get("/signup", authController.getSignup);
+router.get('/login', authController.getLogin);
 
-router.post("/login", authController.postLogin);
+router.get('/signup', authController.getSignup);
 
 router.post(
-  "/signup",
-  [check("email")
-    .isEmail()
-    .withMessage("Enter a valid E-mail address.")
-    .custom((value, { req }) => {
-      // if (value === "test@test.com") {
-      //   throw new Error("This email address if forbidden.");
-      //   }
-      //   return true;
-      User.findOne({ email: value }).then(userDoc => {
-        if (userDoc) {
-          return Promise.reject('E-Mail exists already, pick a different one.')
-        }
-      }).catch(err=>console.log(err))
-    }),
-    body('password', 'Enter a password with only numbers and text and at least 4 characters.').isLength({ min: 4 }).isAlphanumeric(),
+  '/login',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.'),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+  ],
+  authController.postLogin
+);
+
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      }),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
     body('confirmPassword').custom((value, { req }) => {
       if (value !== req.body.password) {
-        throw new Error("Passwords have to match.");
+        throw new Error('Passwords have to match!');
       }
       return true;
-  })],
+    })
+  ],
   authController.postSignup
 );
 
-router.post("/logout", authController.postLogout);
+router.post('/logout', authController.postLogout);
 
-router.get("/reset", authController.getReset);
+router.get('/reset', authController.getReset);
 
-router.post("/reset", authController.postReset);
+router.post('/reset', authController.postReset);
 
-router.get("/reset/:token", authController.getNewPassword);
+router.get('/reset/:token', authController.getNewPassword);
 
-router.post("/new-password", authController.postNewPassword);
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
