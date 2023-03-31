2721dbc21bdea3b99600b5298193286a23baba97
piyush-chaturvedi-au9 <piyush1.chatur@gmail.com>
2023-03-31 21:57:57 +0530

    component splitting

---
+++ reactjs/day14/secondapp/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Cockpit.module.css';
+
+
+const cockpit = (props) => {
+    let btnClass = [classes.Button]
+    if (props.showPersons) {
+        btnClass.push(classes.Red);
+    }
+
+    let assignedClasses = [];
+    if (props.persons.length <= 2) {
+        assignedClasses.push(classes.red); //classes = [red];
+    }
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold); //classes = ['red','bold']
+    }
+
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <p className={assignedClasses.join(' ')}>This is working.!!!</p>
+            <button className={btnClass.join(' ')}
+                onClick={props.clicked}>
+                Toggle Persons
+            </button>
+        </div>
+    );
+}
+
+
+export default cockpit;
